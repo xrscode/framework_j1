38108b5e402be43e2b004b9842060f9bd93437ac
Simon Pollock <sjp@adatis.co.uk>
2025-01-30 11:53:07 +0000

    upload

--- populateDB.py
+++ populateDB.py
@@ -1,28 +1,30 @@
 import json
-from azure.identity import DefaultAzureCredential
+from azure.identity import DefaultAzureCredential, ClientSecretCredential
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.identity import ClientSecretCredential
+from dotenv import load_dotenv
+import os
 from azure.storage.blob import BlobServiceClient
+from azure.core.exceptions import ResourceNotFoundError
 
-try:
-    print("Azure Blob Storage Python quickstart sample")
+# Load the dotenv:
+load_dotenv()
 
-    # Quickstart code goes here
 
-except Exception as ex:
-    print('Exception:')
-    print(ex)
-
-# First Establish Connection Details
+# First Establish Connection Details from Contract:
 with open('./src/contracts/_sourceSystem.json', 'r') as f:
     connectionDetails = json.load(f)
 
+# Define the Connection String and Container Name:
+sas_token = os.getenv("SAS_TOKEN")
+account_url = connectionDetails['connectionDetails']['account_url']
+container_name = connectionDetails['connectionDetails']['containerName']
 
-products_conneciton_string = connectionDetails['connectionDetails']['blobConnectionString']
+# Create a blob service client:
+blob_service_client = BlobServiceClient(account_url=account_url, credential=sas_token)
 
-try:
-    print("Azure Blob Storage Python quickstart sample")
+# Get a container client to interact with the container
+container_client = blob_service_client.get_container_client(container_name)
 
-    # Quickstart code goes here
+print(container_client)
 
-except Exception as ex:
-    print('Exception:')
-    print(ex)
