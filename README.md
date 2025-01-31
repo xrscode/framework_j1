2cef09224104b8f7b042d3ac6b1d257ac7062bbc
Simon Pollock <sjp@adatis.co.uk>
2025-01-31 12:00:50 +0000

    SQL connection

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # framework_j1
 Basic Framework for Learning.
 
+<!-- Driver -->
+Install driver for connection to sql database:
+https://learn.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver16&redirectedfrom=MSDN
+
 <!-- Sql Server Connection Details -->
--- populateDB.py
+++ populateDB.py
@@ -1,11 +1,12 @@
 import json
-from azure.identity import DefaultAzureCredential, ClientSecretCredential
+from azure.identity import DefaultAzureCredential, ClientSecretCredential, InteractiveBrowserCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-from azure.identity import ClientSecretCredential
 from dotenv import load_dotenv
 import os
-from azure.storage.blob import BlobServiceClient
 from azure.core.exceptions import ResourceNotFoundError
+import pandas as pd
+from io import StringIO
+import pyodbc
 
 # Load the dotenv:
 load_dotenv()
@@ -17,14 +18,76 @@ with open('./src/contracts/_sourceSystem.json', 'r') as f:
 
 # Define the Connection String and Container Name:
 sas_token = os.getenv("SAS_TOKEN")
-account_url = connectionDetails['connectionDetails']['account_url']
-container_name = connectionDetails['connectionDetails']['containerName']
+connection_string = connectionDetails['connectionDetails']['connectionString']
+conatinerName = connectionDetails['connectionDetails']['containerName']
+blob_location = connectionDetails['connectionDetails']['fileLocation']
 
-# Create a blob service client:
-blob_service_client = BlobServiceClient(account_url=account_url, credential=sas_token)
 
-# Get a container client to interact with the container
-container_client = blob_service_client.get_container_client(container_name)
+# Create client:
+blob_service_client = BlobServiceClient(account_url=connection_string, credential=sas_token)
 
-print(container_client)
+# Get the blob client for the specific blob (products.csv):
+blob_client = blob_service_client.get_blob_client(container=conatinerName, blob=blob_location)
 
+# Download products.csv:
+blob_data = blob_client.download_blob().readall().decode('utf-8').split('\n')
+
+# Now Begin to build up SQL QUERY:
+
+# Define headers:
+header = ', '.join(blob_data[0].split(','))
+
+
+# Define the values:
+val_str = ""
+values = blob_data
+for value in values[1:]:
+    v = value.split(',')
+    x = [f"'{i}'" for i in v]
+    val_str += f"({', '.join(x)}), \n"
+# Remove final comma:
+val_str = val_str[:-3]
+
+# Construct SQL Query 
+query = f"""INSERT INTO products ({header}) 
+
+VALUES {val_str};"""
+
+
+"""
+The next step is  to connect to the SQL Server/Database.
+"""
+
+# Database connection details:
+server = os.getenv('serverName')
+database = os.getenv('databaseName')
+admin = os.getenv('serverAdmin')
+password = os.getenv('serverPassword')
+connectionString = os.getenv('connectionString')
+
+# # Set connection string:
+# connectionString = f"""Driver={{ODBC Driver 18 for SQL Server}};Server=tcp:framework-j1-sv.database.windows.net,1433;Database=framework-j1-db;Uid=adminsjp;Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"""
+
+# Open Connection:
+conn = pyodbc.connect(connectionString) 
+
+# Create cursor:
+cursor = conn.cursor()
+
+SQL_QUERY = f"""CREATE TABLE Products (
+ProductID int,
+ProductName varchar,
+ProductNumber varchar,
+Category varchar,
+ListPrice varchar
+);"""
+
+try:
+    # Execute the query:
+    cursor.execute(SQL_QUERY)
+except Exception as e:
+    print(e)
+
+# Close connection:
+cursor.close()
+conn.close()
