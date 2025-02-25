46d0d634b1e25e71591f7e4068ec125b78979645
Simon Pollock <sjp@adatis.co.uk>
2025-02-25 10:07:47 +0000

    Removed unnecessary files

--- setup.sh
+++
@@ -1,7 +0,0 @@
-# Deploy Resources:
-# cd terraform && terraform init && terraform plan && terraform apply -auto-approve
-
-# cd ../src/files  
-
-# Setup Schema:
-python ./setupMetadataDatabase.py 
