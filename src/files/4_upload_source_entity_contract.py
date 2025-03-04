9cdc0c4ba4df5e4c332c6b4ddf41f9e482e99f7c
Simon Pollock <sjp@adatis.co.uk>
2025-03-04 14:53:37 +0000

    Updated source entity upload

--- src/files/4_upload_source_entity_contract.py
+++ src/files/4_upload_source_entity_contract.py
@@ -1,56 +1,75 @@
 import json
 from utility_functions import ddl_metadata
 import os
+import inquirer
 
 """
 This python file will take a sourceEntityContract.json and upload it into the database. 
 """
 
-# Set the source system name to be uploaded:
-sourceSystemName = input('Enter the source system name:')
-sourceEntityPath = f'./src/contracts/{sourceSystemName}/'
+# First get list of folders/sourcesystems:
+sourceSystems = [folder for folder in os.listdir('./src/contracts')]
+sourceSystems.append('Exit')
 
-# List all files in the folder
-json_files = [f for f in os.listdir(sourceEntityPath) if f.endswith(".json") and f != "_sourceSystem.json"]
+while True:
+    questions = [
+        inquirer.List('choice',
+                      message="Which source system would you like to upload?",
+                      choices=sourceSystems,
+                      )
+    ]
 
-# Iterate through the contracts and upload them to the database:
-for contract in json_files:
-    # Reconstruct full file path:
-    file = sourceEntityPath + contract
-    # Open contract:
-    with open(file, 'r') as c:
-        d = json.load(c)
-    
-    # Save variables:
-    entityName = d['name']
-    entityDescription = d['description']
-    entitySourceQuery = str(d['connectionDetails']).replace("'", '"')
-    entityColumns = str(d['columns']).replace("'", '"')
-   
-    # Build up query
-    query = f"""
-    --First create varaiable to store sourceEntityID:
-    DECLARE @sourceEntityID INT;
+    sourceSystemName = inquirer.prompt(questions)['choice']
 
-    -- Save the sourceEntityID to variable:
-    SELECT @sourceEntityID = sourceEntityID
-    FROM sourceSystem
-    WHERE sourceEntityName = '{sourceSystemName}';
+    if sourceSystemName == 'Exit':
+        print('Exiting...')
+        break
+    else:
+        sourceEntityPath = f'./src/contracts/{sourceSystemName}/'
 
-    --Delete from sourceEntity If exists:
-    DELETE FROM sourceEntity
-    WHERE entityName = '{sourceSystemName}';
+        # List all files in the folder
+        json_files = [f for f in os.listdir(sourceEntityPath) if f.endswith(".json") and f != "_sourceSystem.json"]
 
-    --Insert data into sourceEntity table:
-    INSERT INTO sourceEntity (sourceEntityID, entityName, entityDescription, entitySourceQuery, entityColumns)
-    VALUES (@sourceEntityID, '{entityName}', '{entityDescription}', '{entitySourceQuery}', '{entityColumns}');"""
+        # Iterate through the contracts and upload them to the database:
+        for contract in json_files:
+            # Reconstruct full file path:
+            file = sourceEntityPath + contract
+            # Open contract:
+            with open(file, 'r') as c:
+                d = json.load(c)
+            
+            # Save variables:
+            entityName = d['name']
+            entityDescription = d['description']
+            entitySourceQuery = str(d['connectionDetails']).replace("'", '"')
+            entityColumns = str(d['columns']).replace("'", '"')
+        
+            # Build up query
+            query = f"""
+            --First create varaiable to store sourceEntityID:
+            DECLARE @sourceEntityID INT;
+
+            -- Save the sourceEntityID to variable:
+            SELECT @sourceEntityID = sourceEntityID
+            FROM sourceSystem
+            WHERE sourceEntityName = '{sourceSystemName}';
+
+            --Delete from sourceEntity If exists:
+            DELETE FROM sourceEntity
+            WHERE entityName = '{sourceSystemName}';
+
+            --Insert data into sourceEntity table:
+            INSERT INTO sourceEntity (sourceEntityID, entityName, entityDescription, entitySourceQuery, entityColumns)
+            VALUES (@sourceEntityID, '{entityName}', '{entityDescription}', '{entitySourceQuery}', '{entityColumns}');"""
 
 
-    # Execute the query:
-    try:
-        print(f'Uploading entity: {entityName}')
-        rowCount = ddl_metadata(query)
-        if rowCount == -1:
-            print(f'Entity: {entityName} upload successful.')
-    except Exception as e:
-        print(f'Error message: {e}')
+            # Execute the query:
+            try:
+                print(f'Uploading entity: {entityName}')
+                rowCount = ddl_metadata(query)
+                if rowCount == -1:
+                    print(f'Entity: {entityName} upload successful.')
+            except Exception as e:
+                print(f'Error message: {e}')
+
+        break
