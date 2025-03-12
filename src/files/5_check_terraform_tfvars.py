6a2ad70bc15f8270928951e2536b02eba7b62e0c
xrscode <xrs@icloud.com>
2025-03-12 11:52:37 +0000

    Made 5_check_terraform autopep8 compliant

--- src/files/5_check_terraform_tfvars.py
+++ src/files/5_check_terraform_tfvars.py
@@ -1,26 +1,24 @@
 import os
-import requests
-import json
 import subprocess
-import sys
 import inquirer
 
 """
 In order for terraform to deploy correctly, terraform.tfvars will need to be created.
-It needs to be populated with GIT credentials. 
+It needs to be populated with GIT credentials.
 """
 
 # Define path to terraform .tfvars:
 path = './terraform/terraform.tfvars'
 
+
 def validate_git_credentials(url: str, pat: str) -> bool:
     """
     This function determines if the supplied url and pat are valid.
 
-    Args: 
+    Args:
         url (str) the url to the valid github repo.
         pat (str) the pat token to allow access to the github repo.
-    
+
     Returns:
         bool: True if url and pat are valid.
 
@@ -33,24 +31,27 @@ def validate_git_credentials(url: str, pat: str) -> bool:
     """
     if not isinstance(url, str):
         raise TypeError('String required for URL.')
-    
+
     if not isinstance(pat, str):
         raise TypeError('String required for PAT.')
-    
-    if not "https://" in url:
+
+    if "https://" not in url:
         raise ValueError('Please supply valid url.  https://...')
-    
+
     # Check PAT:
     try:
-        result = subprocess.run(
-            ["curl", "-s", "-I", "-H", f"Authorization: token {pat}", "https://api.github.com/user"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-            timeout=10
-        )
+        result = subprocess.run(["curl",
+                                 "-s",
+                                 "-I",
+                                 "-H",
+                                 f"Authorization: token {pat}",
+                                 "https://api.github.com/user"],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                text=True,
+                                timeout=10)
         # If invalid pat raise exception:
-        if not "HTTP/1.1 200 OK" in result.stdout and not "HTTP/2 200" in result.stdout:
+        if "HTTP/1.1 200 OK" not in result.stdout and "HTTP/2 200" not in result.stdout:
             raise RuntimeError('Invalid PAT')
 
     except Exception as e:
@@ -71,7 +72,7 @@ def validate_git_credentials(url: str, pat: str) -> bool:
             raise RuntimeError('Invalid url.')
     except Exception as e:
         raise RuntimeError('Invalid url.')
-    
+
     return True, "Valid URL and PAT token."
 
 
@@ -80,17 +81,17 @@ def create_terraform_tfvars(path: str) -> bool:
     """
     This function checks if terraform tfvars exists.
     If it does not exist, it creates it.
-    If it does exit it returns true. 
+    If it does exit it returns true.
 
-    Args: 
+    Args:
         path (str) path to terraform.tfvars.
-       
+
     Returns:
         bool: True if terraform.tfvars exists
         bool: True fi terraform.tfvars successfully created.
 
     Raises:
-        TypeError: if supplied path is not a string. 
+        TypeError: if supplied path is not a string.
     """
 
     # Check path str:
@@ -110,7 +111,7 @@ def create_terraform_tfvars(path: str) -> bool:
 
 def update_terraform_tfvars(path: str):
     """
-    This function reads from terraform tfvars. 
+    This function reads from terraform tfvars.
     It looks for a git_url, git_pat and git_user.
     If they do not exist, it will prompt the user for them.
     """
@@ -118,25 +119,27 @@ def update_terraform_tfvars(path: str):
     # Check path is string:
     if not isinstance(path, str):
         raise TypeError('Path should be string.')
-    
+
     # Read from terraform.tfvars:
     with open(path, 'r+') as f:
         data = f.read()
-        
 
     # Check git_url exists:
-    if not "git_user" in data:
+    if "git_user" not in data:
         git_user = input('Please enter your git user name.')
     # If git_url exists overwrite?
     else:
         # Prompt user to overwrite or not:
         questions = [
-        inquirer.List('choice',
-                      message="Git_user already exists.  Would you like to overwrite?",
-                      choices=['yes', 'no'],
-                      )]
+            inquirer.List(
+                'choice',
+                message="Git_user already exists.  Would you like to overwrite?",
+                choices=[
+                    'yes',
+                    'no'],
+            )]
         answer = inquirer.prompt(questions)
-        
+
         # Prompt to overwrite:
         if answer['choice'] == 'yes':
             # Prompt user to input user:
@@ -146,17 +149,20 @@ def update_terraform_tfvars(path: str):
             git_user = data.split('\n')[0][12:-1]
 
     # Check git_pat exists:
-    if not "git_pat" in data:
+    if "git_pat" not in data:
         git_pat = input('Please enter your git pat token.')
     else:
         # Prompt user to overwrite or not:
         questions = [
-        inquirer.List('choice',
-                      message="Git_pat already exists.  Would you like to overwrite?",
-                      choices=['yes', 'no'],
-                      )]
+            inquirer.List(
+                'choice',
+                message="Git_pat already exists.  Would you like to overwrite?",
+                choices=[
+                    'yes',
+                    'no'],
+            )]
         answer = inquirer.prompt(questions)
-        
+
         # Prompt to overwrite:
         if answer['choice'] == 'yes':
             # Prompt user to input user:
@@ -164,20 +170,22 @@ def update_terraform_tfvars(path: str):
         else:
             # Grab existing git_user:
             git_pat = data.split('\n')[1][11:-1]
-        
 
     # Check git_url exists:
-    if not "git_url" in data:
+    if "git_url" not in data:
         git_url = input('Please enter your git url.')
     else:
         # Prompt user to overwrite or not:
         questions = [
-        inquirer.List('choice',
-                      message="Git_url already exists.  Would you like to overwrite?",
-                      choices=['yes', 'no'],
-                      )]
+            inquirer.List(
+                'choice',
+                message="Git_url already exists.  Would you like to overwrite?",
+                choices=[
+                    'yes',
+                    'no'],
+            )]
         answer = inquirer.prompt(questions)
-        
+
         # Prompt to overwrite:
         if answer['choice'] == 'yes':
             # Prompt user to input user:
@@ -187,7 +195,7 @@ def update_terraform_tfvars(path: str):
             git_url = data.split('\n')[2][11:-1]
 
     # Write variables to terraform.tfvars:
-    try:  
+    try:
         with open(path, 'w') as f:
             # Write the user credentials to the file:
             f.write(f'git_user = "{git_user}"\n')
@@ -196,7 +204,7 @@ def update_terraform_tfvars(path: str):
         return True, 'Terraform.tfvars now exists with variables.'
     except Exception as e:
         print('Error: ', e)
-    
+
 
 def read_terraform_tfvars(path):
     with open(path, 'r') as f:
@@ -205,7 +213,6 @@ def read_terraform_tfvars(path):
         git_pat = data.split('\n')[1][11:-1]
     return [git_url, git_pat]
 
-    
 
 # Create terraform tfvars if does not exist:
 create_terraform_tfvars(path)
@@ -215,11 +222,3 @@ update_terraform_tfvars(path)
 git_url, git_pat = read_terraform_tfvars(path)
 # # Check credentials valid:
 validate_git_credentials(git_url, git_pat)
-
-
-
-
-
-
-
-
