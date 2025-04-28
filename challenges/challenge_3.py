bc57336f1fae546a31e8b3f79853dec51bd0da10
xrscode <xrs@icloud.com>
2025-04-28 13:17:30 +0100

    Added challenge 3

---
+++ challenges/challenge_3.py
@@ -0,0 +1,14 @@
+welcome_message = """
+
+Query the Metadata Database
+-------------------------------------------------------------------------------
+
+By now, you should have successfully generated contracts and uploaded them
+into the metadata database. 
+
+Your next challenge is to write a query that queries the metadata database.
+
+Carefully follow the instructions in this script to complete the task.
+"""
+
+print(welcome_message)
