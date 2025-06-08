43054c2853b88bdb82ca5dfd384aff932eb73939
xrscode <xrs@icloud.com>
2025-06-08 09:41:02 +0100

    Adding broken pipeline challenge

--- challenges/challenge_5.py
+++ challenges/challenge_5.py
@@ -3,41 +3,24 @@ from src.files.utility_functions import load_dotenv, get_secret_from_keyvault,\
 create_data_lake_directory_client, get_current_date_path, delete_directory
 
 """
-In this challenge, run this script.
+-----------------------------------------------------------------------------
+BROKEN PIPELINE
+-----------------------------------------------------------------------------
 
-After you have run it, in ADF run the pipeline for AdventureWorks again, to 
-see what has happened!
+Uh oh...  The first rule of being a developer is to never make a 
+MISTKAE!
+That means that you will never have to fix anything - a real time saver!
 
-You can then run pytest to see what is happening:
----------------------------------------------------------------------------
-pytest -vv ./challenges/tests/test_challenge_4.py
----------------------------------------------------------------------------
+It seems that a plucky young developer has created a partially working
+ADF pipeline.  The problem?  It's not working as it should...
 
+Perhaps you could lend a hand?
+
+Open ADF and look at the pipeline editor.  There is a pipeline called:
+'Broken Pipeline'. 
+
+Run it and see if you can fix it!
 """
 
-# Refresh dotenv:
-load_dotenv(override = True)
-
-# Keyvault name:
-keyvault_name = os.getenv('keyvault_name')
-
-# Define sas token:
-sas_token = get_secret_from_keyvault(keyvault_name, 'sastoken')
-
-# Define account url:
-account_url = os.getenv('account_url')
-
-# Define the expected directory locations:
-expected_file_locations_bronze = [
-f'/framework-j1/BRONZE/AdventureWorks/customer_AW/{get_current_date_path()}',
-f'/framework-j1/BRONZE/AdventureWorks/products_AW/{get_current_date_path()}',
-f'/framework-j1/BRONZE/AdventureWorks/sales_order_AW/{get_current_date_path()}'
-]
-
-# Iterate through expected_file_locations:
-for loc in expected_file_locations_bronze:
-    # Create client for location:
-    client = create_data_lake_directory_client(account_url, loc, sas_token)
-    delete_directory(client)
 
     
